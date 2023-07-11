aee0de45a50d50e829649356b3667c421d66dab9
MeLook1 <andruxa_099@mail.ru>
2023-07-11 14:48:04 +0300

    del шрифты, изменил кнопки на радио, кривой check

--- components/lvl-page-component.js
+++ components/lvl-page-component.js
@@ -4,9 +4,12 @@ export function renderLevelPageComponent({ appEl }) {
                     <div class="lvl-box"> 
                     <div class="lvl-box-title">Выбери сложность</div> 
                     <div class="lvl-box-numbers">
-                    <button class="lvl-number">1</button>
-                    <button class="lvl-number">2</button>
-                    <button class="lvl-number">3</button>
+                    <input type="radio" id="radioButLvL1" value="LvL1">
+                        <label for="radioButLvL1">1</label>
+                    <input type="radio" id="radioButLvL2" value="LvL2">
+                        <label for="radioButLvL2">2</label>
+                    <input type="radio" id="radioButLvL3" value="LvL3">
+                        <label for="radioButLvL3">3</label>
                     </div>
                     <button class="start-button">Старт</button>
                     </div>
--- global.js
+++ global.js
@@ -1,10 +1,10 @@
 import { LVL_PAGE } from "./pages.js";
 import { renderLevelPageComponent } from "./components/lvl-page-component.js";
 
-let page = null;
+let page = LVL_PAGE;
 const renderApp = () => {
     const appEl = document.getElementById("app");
-    if (page = LVL_PAGE) {
+    if (page === LVL_PAGE) {
         return renderLevelPageComponent({ appEl });
     }
 };
