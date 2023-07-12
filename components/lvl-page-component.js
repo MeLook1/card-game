d87d532f88dddefd494c49547c3a150b3f88764a
MeLook1 <andruxa_099@mail.ru>
2023-07-12 18:55:53 +0300

    верстка страницы уровней работает

--- components/lvl-page-component.js
+++ components/lvl-page-component.js
@@ -4,11 +4,11 @@ export function renderLevelPageComponent({ appEl }) {
                     <div class="lvl-box"> 
                     <div class="lvl-box-title">Выбери сложность</div> 
                     <div class="lvl-box-numbers">
-                    <input type="radio" id="radioButLvL1" value="LvL1">
+                    <input type="radio" id="radioButLvL1" value="LvL1" name="radioBtn">
                         <label for="radioButLvL1">1</label>
-                    <input type="radio" id="radioButLvL2" value="LvL2">
+                    <input type="radio" id="radioButLvL2" value="LvL2" name="radioBtn">
                         <label for="radioButLvL2">2</label>
-                    <input type="radio" id="radioButLvL3" value="LvL3">
+                    <input type="radio" id="radioButLvL3" value="LvL3" name="radioBtn">
                         <label for="radioButLvL3">3</label>
                     </div>
                     <button class="start-button">Старт</button>
